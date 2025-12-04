6395a2c516d52bfa985664506ed6640735e99ba8
Mark Garcia <111922999+MarkRyanGarcia@users.noreply.github.com>
2025-12-03 22:07:28 -0800

    day04: cleanup

--- 2025/day04/sol.py
+++ 2025/day04/sol.py
@@ -5,18 +5,19 @@ directions = [
     (-1,  0),          (1,  0),
     (-1,  1), (0,  1), (1,  1)
     ]
-    
+
 grid = []
 
 def part1(g):
     res = 0
-    for r in range(len(g)):
-        for c in range(len(g[0])):
+    m, n = len(g), len(g[0])
+    for r in range(m):
+        for c in range(n):
             if g[r][c] == '@':
                 neighbors = 0
                 for dx, dy in directions:
                     i, j = r+dx, c+dy
-                    if i in range(len(g)) and j in range(len(g[0])) and g[i][j] == '@':
+                    if i in range(m) and j in range(n) and g[i][j] == '@':
                         neighbors += 1
                 if neighbors < 4:
                     res += 1
@@ -24,19 +25,20 @@ def part1(g):
 
 def part2(g):
     res = 0
-    temp2 = deepcopy(g)
-    for r in range(len(g)):
-        for c in range(len(g[0])):
+    m, n = len(g), len(g[0])
+    temp = deepcopy(g)
+    for r in range(m):
+        for c in range(n):
             if g[r][c] == '@':
                 neighbors = 0
                 for dx, dy in directions:
                     i, j = r+dx, c+dy
-                    if i in range(len(g)) and j in range(len(g[0])) and (g[i][j] == '@'):
+                    if i in range(m) and j in range(n) and (g[i][j] == '@'):
                         neighbors += 1
                 if neighbors < 4:
                     res += 1
                     g[r][c] = 'x'
-    return res + part2(g) if temp2 != g else res
+    return res + part2(g) if temp != g else res
 
 with open("in.txt", 'r') as file:
     for line in file:
