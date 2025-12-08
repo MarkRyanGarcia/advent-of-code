e10d0abb78b13bbe60aa90a9eed2d8e412678972
Mark Garcia <111922999+MarkRyanGarcia@users.noreply.github.com>
2025-12-07 18:02:08 -0800

    2025 day07: do dp in-place instead of separate

--- 2025/day07/sol.py
+++ 2025/day07/sol.py
@@ -9,12 +9,12 @@ def part1(grid):
     while row in range(m-1):
         next_beams = []
         for r, c in beams:
-            if grid[r+1][c] == '.':
+            below = grid[r+1][c]
+            if below == '.':
                 grid[r+1][c] = '|'
                 next_beams.append((r+1,c))
-            elif grid[r+1][c] == '^':
+            elif below == '^':
                 grid[r+1][c-1] = '|'
-                grid[r+1][c+1] = '|'
                 next_beams.append((r+1,c-1))
                 next_beams.append((r+1,c+1))
                 res += 1
@@ -24,25 +24,35 @@ def part1(grid):
 
 def part2(grid) -> int:
     m, n = len(grid), len(grid[0])
-    s_idx = n//2
-    grid[1][s_idx] = '|'
-    counts = [[0] * n for _ in range(m)]
-    counts[1][s_idx] = 1
-
+    grid[1][n//2] = 1
     for r in range(1, m-1):
         for c in range(n):
-            i = counts[r][c]
+            i = grid[r][c] if type(grid[r][c]) == int else 0
             if i == 0:
                 continue
             below = grid[r+1][c]
-            if below == '.':
-                counts[r+1][c] += i
+            if below != '^':
+                if type(grid[r+1][c]) == int:
+                    grid[r+1][c] += i
+                else:
+                    grid[r+1][c] = i
             elif below == '^':
                 if c-1 in range(n):
-                    counts[r+1][c-1] += i
+                    if type(grid[r+1][c-1]) == int:
+                        grid[r+1][c-1] += i
+                    else:
+                        grid[r+1][c-1] = i
                 if c+1 in range(n):
-                    counts[r+1][c+1] += i
-    return sum(counts[m-1])
+                    if type(grid[r+1][c+1]) == int:
+                        grid[r+1][c+1] += i
+                    else:
+                        grid[r+1][c+1] = i
+    return sum([x for x in grid[m-1] if type(x) == int])
+
+def printgrid(grid):
+    for row in grid:
+        r = [str(x) for x in row]
+        print(r)
 
 lines = open("in.txt").readlines()
 grid = []
